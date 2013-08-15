e3efc954a59450c622a257d65aded84bd0474dff
Sumobi <andrew@sumobi.com>
2013-08-15 13:42:35 +1200

    1.1

--- edd-hide-download.php
+++ edd-hide-download.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Easy Digital Downloads - Hide Download
-Plugin URI: http://sumobi.com/shop/hide-download/
-Description: Prevents a download appearing on the custom post type archive page or [downloads] listing.
-Version: 1.0
+Plugin URI: http://sumobi.com/shop/edd-hide-download/
+Description: Prevents a download from appearing on the custom post type archive page or wherever the [downloads] shortcode is used. Direct access to a download can also be disabled and the user will be redirected to the homepage.
+Version: 1.1
 Author: Andrew Munro, Sumobi
 Author URI: http://sumobi.com/
 License: GPL-2.0+
@@ -25,27 +25,20 @@ if ( !class_exists( 'EDD_Hide_Download' ) ) {
 
 		function __construct() {
 			add_action( 'init', array( $this, 'textdomain' ) );
-			add_action( 'edd_meta_box_fields', array( $this, 'add_metabox' ), 10 );
+			add_action( 'edd_meta_box_fields', array( $this, 'add_metabox' ), 100 );
 			add_action( 'edd_metabox_fields_save', array( $this, 'save_metabox' ) );
 			add_action( 'pre_get_posts',  array( $this, 'pre_get_posts' ) );
 			add_filter( 'edd_downloads_query', array( $this, 'shortcode_query' ) );
 
 			// find all hidden products on metabox render
 			add_action( 'edd_meta_box_fields', array( $this, 'query_hidden_downloads' ), 90 );
+
 			// redirect if product is set to be hidden
-			add_action( 'wp_head', array( $this, 'redirect_hidden' ) );
+			add_action( 'template_redirect', array( $this, 'redirect_hidden' ) );
+
 			// load the hidden downloads
 			$this->hidden_downloads = get_option( 'edd_hd_ids', array() );
-		}
 
-		/**
-		 * Constants
-		 *
-		 * @since 1.0
-		*/
-		function constants() {
-			if ( !defined( 'EDD_HD_VERSION' ) )
-				define( 'EDD_HD_VERSION', '1.0' );
 		}
 
 		/**
@@ -66,22 +59,25 @@ if ( !class_exists( 'EDD_Hide_Download' ) ) {
 			$checked = (boolean) get_post_meta( $post_id, '_edd_hide_download', true );
 			$is_redirect = (boolean) get_post_meta( $post_id, '_edd_hide_redirect_download', true );
 		?>
+			<p><strong><?php apply_filters( 'edd_hide_download_header', printf( __( 'Hide %s', 'edd-hd' ), edd_get_label_singular() ) ); ?></strong></p>
 			<p>
 				<label for="edd_hide_download">
 					<input type="checkbox" name="_edd_hide_download" id="edd_hide_download" value="1" <?php checked( true, $checked ); ?> />
-					<?php printf( __( 'Hide %s', 'edd-hd' ), edd_get_label_singular() ); ?>
+					<?php apply_filters( 'edd_hide_download_label', printf( __( 'Hide this %s from appearing on the custom post type archive page and wherever the [downloads] shortcode is used', 'edd-hd' ), edd_get_label_singular() ) ); ?>
+				
 				</label>
 			</p>
-			
 			<p>
 				<label for="edd_hide_redirect_download">
 					<input type="checkbox" name="_edd_hide_redirect_download" id="edd_hide_redirect_download" value="1" <?php checked( true, $is_redirect ); ?> />
-					<?php printf( __( 'Disable direct access to %s', 'edd-hd' ), edd_get_label_singular() ); ?>
+						<?php apply_filters( 'edd_hide_download_disable_access_label', printf( __( 'Disable direct access to this %s', 'edd-hd' ), edd_get_label_singular() ) ); ?>
+
 				</label>
 			</p>
 
 		<?php
 		}
+
 
 		/**
 		 * Add to save function
@@ -98,6 +94,7 @@ if ( !class_exists( 'EDD_Hide_Download' ) ) {
 
 		/**
 		 * Store the hidden products ids in the options table
+		 *  @since 1.1
 		 */
 		function query_hidden_downloads() {
 			
@@ -110,9 +107,10 @@ if ( !class_exists( 'EDD_Hide_Download' ) ) {
 
 			$hidden_downloads = array();
 
-			foreach ($downloads as $download) {
+			foreach ( $downloads as $download ) {
 				$hidden_downloads[] = $download->ID;
 			}
+
 			update_option( 'edd_hd_ids', $hidden_downloads );
 
 		}
@@ -156,32 +154,37 @@ if ( !class_exists( 'EDD_Hide_Download' ) ) {
 
 		}
 
+
 		/**
 		 * Redirect if product needs to be hidden
-		 * 
+		 *  @since 1.1
 		 */
 		function redirect_hidden() {
 			global $post;
 
-			if ( ! in_array( $post->ID, $this->hidden_downloads ) )
-				return;
+			 if ( ! in_array( $post->ID, $this->hidden_downloads ) )
+			 	return;	
 
-			$is_redirect_active = (boolean) get_post_meta( $post->ID, '_edd_hide_redirect_download', true );
+			 $is_redirect_active = (boolean) get_post_meta( $post->ID, '_edd_hide_redirect_download', true );
 
-			if ( $is_redirect_active ) {
+			 if ( $is_redirect_active ) {
 
 				$redirect_url = site_url();
 
-				if ( isset( $_REQUEST['HTTP_REFERER '] ) ) {
+				if ( isset( $_REQUEST['HTTP_REFERER'] ) ) {
 					$referer = esc_url( $_REQUEST['HTTP_REFERER '] );
+
 					if ( strpos( $referer, $redirect_url ) !== false )
 						$redirect_url = $referer;
 				}
 
-				wp_redirect( $redirect_url, 301 );
+				wp_redirect( $redirect_url, 301 ); exit;
+				
 			}
+			
 		}
-	}
-}
 
+	}
+
+}
 $EDD_Hide_Download = new EDD_Hide_Download();
